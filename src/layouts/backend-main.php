f7c8f5890cc0cf9f37a6728a7e25c3cb1eddf207
coderbaha <bahaask41@gmail.com>
2022-02-26 12:02:20 +0300

    updated

--- src/layouts/backend-main.php
+++ src/layouts/backend-main.php
@@ -33,7 +33,7 @@ $languages  = json_decode(Setting::findOne(['name' => 'app::language'])->config,
 <div class="wrap">
 <?php
     NavBar::begin([
-        'brandLabel' => Html::img(Yii::$app->request->baseUrl.'/data/'.strval(Html::encode($settings['page::logo'])),['width' => 'height']),
+        'brandLabel' => Html::img(Yii::$app->request->baseUrl.'/data/'.strval(Html::encode($settings['page::logo'])),['height' => '30px']),
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
