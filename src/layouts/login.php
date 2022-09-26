d92efbd0a6dd979e1c60e0b8f25e79d3d21accf6
Uğur YILDIZ <yildiz@diginova.com.tr>
2022-09-26 16:58:20 +0300

    fix: resolve bootstrap bug

---
+++ src/layouts/login.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\ArrayHelper;
+use portalium\theme\Theme;
+use portalium\theme\Module;
+use portalium\theme\helpers\Html;
+use portalium\theme\widgets\Alert;
+use portalium\theme\widgets\NavBar;
+use portalium\theme\bundles\AppAsset;
+use portalium\theme\widgets\Breadcrumbs;
+use portalium\site\models\Setting;
+use portalium\site\widgets\Language;
+use portalium\menu\models\Menu;
+use portalium\menu\widgets\Nav;
+
+Theme::registerAppAsset($this);
+
+$settings   = ArrayHelper::map(Setting::find()->asArray()->all(),'name','value');
+$languages  = json_decode(Setting::findOne(['name' => 'app::language'])->config,true);
+
+?>
+<?php $this->beginPage() ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Uğur YILDIZ, Portalium Contributors">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+    
+  </head>
+  <body>
+  <?php $this->beginBody() ?>
+    <main class="form-signin w-100 m-auto">
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </main>
+<?php $this->endBody() ?>
+  </body>
+</html>
+<?php $this->endPage() ?>
