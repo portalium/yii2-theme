74b8772fecac9af71308a25d520225a132251a81
demirmehmet0 <demirmehmet0@yahoo.com>
2023-01-25 22:57:07 +0300

    refactor: add theme component

--- src/Module.php
+++ src/Module.php
@@ -11,6 +11,15 @@ class Module extends \portalium\base\Module
         ]);
     }
 
+    public function registerComponents()
+    {
+        return [
+            'theme' => [
+                'class' => 'portalium\theme\Theme',
+            ],
+        ];
+    }
+
     public static function t($message, array $params = [])
     {
         return parent::coreT('theme', $message, $params);
