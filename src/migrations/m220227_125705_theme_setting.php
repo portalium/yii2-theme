b70c11ace72122ab4df7ac47b522f075fc78c734
demirmehmet0 <demirmehmet0@yahoo.com>
2023-03-30 10:41:13 +0300

    feat: add menu specific

---
+++ src/migrations/m220227_125705_theme_setting.php
@@ -0,0 +1,78 @@
+<?php
+
+use portalium\db\Migration;
+use portalium\menu\models\Menu;
+use portalium\site\Module as SiteModule;
+use portalium\site\models\Form;
+
+class m220227_125705_theme_setting extends Migration
+{
+    public function up()
+    {
+        $this->insert(SiteModule::$tablePrefix . 'setting', [
+            'module' => 'menu',
+            'name' => 'main::menu',
+            'label' => 'Active Web Menu',
+            'value' => 'web-menu',
+            'type' => Form::TYPE_DROPDOWNLIST,
+            'config' => json_encode([
+                'model' => [
+                    'class' => 'portalium\menu\models\Menu', 
+                    'map' => [
+                        'key' => 'slug' ,
+                        'value' => 'name'
+                    ],
+                    'where' => [
+                        'type' => Menu::TYPE['web']
+                    ]
+                ]
+            ])
+        ]);
+
+        $this->insert(SiteModule::$tablePrefix . 'setting', [
+            'module' => 'menu',
+            'name' => 'site::menu',
+            'label' => 'Active Site Menu',
+            'value' => 'web-menu',
+            'type' => Form::TYPE_DROPDOWNLIST,
+            'config' => json_encode([
+                'model' => [
+                    'class' => 'portalium\menu\models\Menu', 
+                    'map' => [
+                        'key' => 'slug' ,
+                        'value' => 'name'
+                    ],
+                    'where' => [
+                        'type' => Menu::TYPE['web']
+                    ]
+                ]
+            ])
+        ]);
+
+        $this->insert(SiteModule::$tablePrefix . 'setting', [
+            'module' => 'menu',
+            'name' => 'menu::mobile',
+            'label' => 'Active Mobile Menu',
+            'value' => 'mobile-menu',
+            'type' => Form::TYPE_DROPDOWNLIST,
+            'config' => json_encode([
+                'model' => [
+                    'class' => 'portalium\menu\models\Menu', 
+                    'map' => [
+                        'key' => 'slug' ,
+                        'value' => 'name'
+                    ],
+                    'where' => [
+                        'type' => Menu::TYPE['mobile']
+                    ]
+                ]
+            ])
+        ]);
+        
+    }
+
+    public function down()
+    {
+        $this->delete(SiteModule::$tablePrefix . 'setting', ['module' => 'menu']);
+    }
+}
