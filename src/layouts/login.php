d43b42e0e42f16b1c16fdf2f819b93a243e866d1
demirmehmet0 <demirmehmet0@yahoo.com>
2023-04-05 13:40:54 +0300

    style: update layout

--- src/layouts/login.php
+++ src/layouts/login.php
@@ -1,35 +1,75 @@
 <?php
 
-use portalium\theme\Theme;
-use portalium\theme\helpers\Html;
-use portalium\theme\widgets\Alert;
 
+use yii\helpers\ArrayHelper;
+
+use portalium\theme\Theme;
+use portalium\theme\Module;
+use portalium\theme\helpers\Html;
+use portalium\site\models\Setting;
+use portalium\site\widgets\Brand;
+use portalium\theme\widgets\Alert;
+use portalium\theme\widgets\NavBar;
+use portalium\theme\bundles\AppAsset;
+use portalium\theme\widgets\Breadcrumbs;
+use portalium\menu\widgets\Nav;
+use portalium\menu\models\Menu;
 
 Theme::registerAppAsset($this);
 
-$languages  = Yii::$app->setting->getConfig('app::language');
-
 ?>
 <?php $this->beginPage() ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Uğur YILDIZ, Portalium Contributors">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-    
-  </head>
-  <body>
-  <?php $this->beginBody() ?>
-    <main class="form-signin w-100 m-auto">
+</head>
+<body>
+<?php $this->beginBody() ?>
+<div class="wrap">
+    <div class="container">
+
+        <?php NavBar::begin([
+            'brandLabel' => Brand::widget(['options' => ['height' => '30px']]),
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar navbar-expand-lg fixed-top navbar-dark bg-dark pt-0 pb-0',
+            ],
+        ]);?>
+
+        <?= Nav::widget([
+            'id' => Yii::$app->setting->getValue('theme::main_menu'),
+            'options' => ['class' => 'navbar-nav ms-auto']
+        ]) ?>
+
+        <?php NavBar::end(); ?>
+
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+
+            <?= Breadcrumbs::widget([
+                'links' => !empty($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ])?>
+
+        </nav>
+
         <?= Alert::widget() ?>
         <?= $content ?>
-    </main>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; Portalium <?= date('Y') ?></p>
+        <p class="pull-right">DigiNova</p>
+    </div>
+</footer>
+
 <?php $this->endBody() ?>
-  </body>
+</body>
 </html>
 <?php $this->endPage() ?>
