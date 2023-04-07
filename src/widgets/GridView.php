90cc09de8535251defeb0a702c71df67edb4e063
demirmehmet0 <demirmehmet0@yahoo.com>
2023-04-07 18:47:10 +0300

    refactor: change widgets path

--- src/widgets/GridView.php
+++ src/widgets/GridView.php
@@ -1,51 +1,15 @@
 <?php
 namespace portalium\theme\widgets;
 
+use portalium\grid\GridView as GridGridView;
 use Yii;
 
-class GridView extends \yii\grid\GridView
+class GridView extends GridGridView
 {
-    public $paginationParams;
+    
     public function init()
     {
         parent::init();
-        $this->pager = [
-            'class' => 'yii\bootstrap5\LinkPager',
-            'options' => [
-                'class' => 'pagination justify-content-end',
-            ],
-        ];
-
-        $this->layout = "{items}<div class='row'><div class='col-md-6'>{summary}</div><div class='col-md-6'>{pager}</div></div>";
     }
 
-    public function renderPager()
-    {
-        $pager = parent::renderPager();
-
-        if ($this->paginationParams && isset($this->paginationParams['urlParams'])) {
-            $urlParams = $this->paginationParams['urlParams'];
-            $anchorParams = [];
-
-            foreach ((array) $urlParams as $key => $value) {
-                if ($key === '#') {
-                    $anchorParams[] = $value;
-                } else {
-                    $urlSeparator = (strpos($pager, '?') !== false) ? '&' : '?';
-                    $pager = preg_replace('/href="([^"]*)"/', 'href="$1' . $urlSeparator . $key . '=' . $value . '"', $pager);
-                }
-            }
-
-            if (!empty($anchorParams)) {
-                $anchorParams = '#' . implode(',', $anchorParams);
-                $pager = preg_replace('/href="([^"]*)"/', 'href="$1' . $anchorParams . '"', $pager);
-            }
-        }
-
-        return $pager;
-    }
-
-
-
-    
 }
