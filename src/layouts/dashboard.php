4ddf4b60ec6d99d9b77f4b40fafd09046538ac4a
Ege Özeren <egetierb@gmail.com>
2023-04-25 16:00:56 +0300

    style: responsive design fix for mobile, desktop

--- src/layouts/dashboard.php
+++ src/layouts/dashboard.php
@@ -30,26 +30,26 @@ Theme::registerAppAsset($this);
 
     <header class="navbar navbar-dark sticky-top flex-md-nowrap p-0 shadow d-flex" style="background-color: #000000 !important;">
         <nav class="navbar navbar-expand-lg navbar-light h-100 p-0 col-md-8 w-100">
-            <div class="container-fluid h-100 p-0 black ">
+            <div class="container-fluid h-100 p-0">
                 <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon" style="filter: invert(48%) sepia(13%) saturate(3207%) hue-rotate(130deg) brightness(95%) contrast(80%);"></span>
+                <span class="navbar-toggler-icon color-filter"></span>
             </button>
-                <div class="row col-md-4 justify-content-around align-items-center">
-                <a class="navbar-brand col-md-4 col-lg-2 me-0 px-3 fs-6 d-flex justify-content-center" style="color:white" href="<?=Yii::$app->homeUrl?>">
+                <!-- <div class="row col-md-4 justify-content-around align-items-center"> -->
+                <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 d-flex ms-auto ms-md-0 justify-content-center" style="color:white" href="<?=Yii::$app->homeUrl?>">
                     <?=Brand::widget(['title' => true, 'options' => ['height' => '30px']])?>
                 </a>
-                <div class="px-4 text-light col-md-3 col-lg-2 me-0 px-3 fs-6 d-flex justify-content-center mt-1 mb-1">
+                <div class="px-4 text-light col-md-3 col-lg-2 me-0 px-3 fs-6 d-flex justify-content-center mt-1 mb-1 d-none d-md-block d-lg-block">
                     <?=Html::encode($this->title)?>
                 </div>
-            </div>
-                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon" style="filter: invert(48%) sepia(13%) saturate(3207%) hue-rotate(130deg) brightness(95%) contrast(80%);"></span>
+            <!-- </div> -->
+                <button class="navbar-toggler collapsed ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon color-filter"></span>
                 </button>
                 <div class="navbar-collapse collapse justify-content-end" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <?=Nav::widget([
                             'id' => Yii::$app->setting->getValue('theme::main_menu'),
-                            'options' => ['class' => 'nav nav-pills flex-shrink-0 dropdown mobile-column'],
+                            'options' => ['class' => 'nav nav-pills flex-shrink-0 dropdown mobile-column justify-content-center'],
                         ])?>
                     </div>
                 </div>
@@ -63,7 +63,7 @@ Theme::registerAppAsset($this);
 
     <div class="container-fluid">
         <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block text-bg-dark ">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block text-bg-dark collapse">
                 <div class="position-sticky pt-2 sidebar-sticky">
                     <?=Nav::widget([
                             'id' => Yii::$app->setting->getValue('theme::side_menu'),
@@ -72,6 +72,9 @@ Theme::registerAppAsset($this);
                     ?>
                 </div>
             </nav>
+            <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4 d-flex justify-content-center align-items-center border-top border-secondary d-md-none d-lg-none title">
+                        <?=Html::encode($this->title)?>
+            </div>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
