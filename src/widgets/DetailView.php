e436825e91da10fea51e13a2ced3c9da80c576e4
KerimDogan149 <kerimdogan0102@gmail.com>
2025-02-23 06:26:01 +0300

    detailviewUpdate

--- src/widgets/DetailView.php
+++ src/widgets/DetailView.php
@@ -7,4 +7,32 @@ class DetailView extends \yii\widgets\DetailView
     {
         parent::init();
     }
+    protected function renderAttribute($attribute, $index)
+    {
+        if (is_string($this->template)) {
+
+            $captionOptions = isset($attribute['captionOptions'])
+                ? Html::renderTagAttributes($attribute['captionOptions'])
+                : '';
+
+
+            
+            $contentOptionsArray = isset($attribute['contentOptions']) ? $attribute['contentOptions'] : [];
+            
+            $contentOptionsArray['style'] = 'overflow-wrap: anywhere;';
+
+            $contentOptions = Html::renderTagAttributes($contentOptionsArray);
+
+            $value = $this->formatter->format($attribute['value'], $attribute['format']);
+
+            return strtr($this->template, [
+                '{label}' => $attribute['label'],
+                '{value}' => $value,
+                '{captionOptions}' => $captionOptions,
+                '{contentOptions}' => $contentOptions,
+            ]);
+        }
+
+        return call_user_func($this->template, $attribute, $index, $this);
+    }
 }
