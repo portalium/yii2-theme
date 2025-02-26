e09ca6faec61b21866b5a6210e86ba7e95ee7163
KerimDogan149 <kerimdogan0102@gmail.com>
2025-02-26 16:25:56 +0300

    feat: attributes add overflowwrap

--- src/widgets/DetailView.php
+++ src/widgets/DetailView.php
@@ -1,10 +1,22 @@
 <?php
+
 namespace portalium\theme\widgets;
+
+use yii\helpers\ArrayHelper;
 
 class DetailView extends \yii\widgets\DetailView
 {
     public function init()
     {
         parent::init();
+
+        foreach ($this->attributes as &$attribute) {
+            if (is_array($attribute)) {
+                $attribute['contentOptions'] = ArrayHelper::merge(
+                    $attribute['contentOptions'] ?? [],
+                    ['style' => 'overflow-wrap: anywhere;']
+                );
+            }
+        }
     }
 }
